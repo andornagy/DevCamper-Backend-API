a85c0365610f2d5950fbbd3d245120e779fb8bf7
Andor Nagy <andornagy@outlook.com>
2025-01-05 19:51:31 +0200

    Folder structuring, morgan middleware

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Load middleware
+const morgan = require("morgan");
+
+// Load routes
+const bootcamps = require("./routes/bootcamps");
+
 //  Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
+// Mount routes
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
