3eac6b57520ed32cb722c298c7efe0925b3981fb
Andor Nagy <andornagy@outlook.com>
2025-01-26 00:46:14 +0200

    user update password / details

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const {
 	forgotPassword,
 	resetPassword,
 	updateDetails,
+	updatePassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -17,6 +18,7 @@ router.route("/login").post(login);
 router.route("/me").get(protect, getMe);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:resettoken").put(resetPassword);
-router.route("/update").put(protect, updateDetails);
+router.route("/updatedetails").put(protect, updateDetails);
+router.route("/updatepassword").put(protect, updatePassword);
 
 module.exports = router;
