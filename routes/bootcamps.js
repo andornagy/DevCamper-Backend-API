8363f090f14be323488daf7afc30c941532a8b56
Andor Nagy <andornagy@outlook.com>
2025-01-26 01:54:08 +0200

    Reviews add/view

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -12,15 +12,18 @@ const {
 const Bootcamp = require("../models/Bootcamp");
 
 // Include other resource routers
-const course = require("./courses");
+const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 const router = express.Router();
 
+// Include middleware
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
-router.use("/:bootcampId/courses", course);
+router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 // Handle all bootcamps
 router
